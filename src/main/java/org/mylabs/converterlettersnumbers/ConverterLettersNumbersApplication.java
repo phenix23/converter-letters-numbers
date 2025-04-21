a4fc596f6ddb368d91a634b464fa897691e7a49b
pheonix_23 <hedahdia.faycel@gmail.com>
2025-04-21 17:01:28 +0200

    Initial commit

---
+++ src/main/java/org/mylabs/converterlettersnumbers/ConverterLettersNumbersApplication.java
@@ -0,0 +1,13 @@
+package org.mylabs.converterlettersnumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterLettersNumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverterLettersNumbersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mylabs/converterlettersnumbers/ConverterLettersNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mylabs.converterlettersnumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterLettersNumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
